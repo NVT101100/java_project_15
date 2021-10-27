49e7816c92e8c660a509dfc967bdee399a80e588
NVT101100 <nguyenvanthoi.1011.bd@gmail.com>
2021-10-27 10:39:22 +0700

    Ngày 27/10 xong chức năng đăng kí

--- SocialNetwork/src/main/java/com/SocialNetwork/Controller/RegisterController.java
+++ SocialNetwork/src/main/java/com/SocialNetwork/Controller/RegisterController.java
@@ -2,12 +2,11 @@ package com.SocialNetwork.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.SocialNetwork.Entity.User;
 import com.SocialNetwork.Repository.UserRepository;
@@ -25,21 +24,27 @@ public class RegisterController {
 		return "user/register";
 	}
 
-	@RequestMapping(value = "/adduser",method = RequestMethod.GET)
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String adduser(@RequestParam("fullName") String name, @RequestParam("email") String email,
-			@RequestParam("password") String password,
-			@RequestParam("gender") String gender) {
-		//User user = new User();
-		
-		/*PasswordEncoder passwordEncoder = new PasswordEncoder();
+			@RequestParam("password") String password, @RequestParam("gender") String gender, Model model) {
+		User user = new User();
+
+		PasswordEncoder passwordEncoder = new PasswordEncoder();
 		user.setUser_password(passwordEncoder.passwordEncoder(password));
 		user.setFullname(name);
 		user.setSex(gender);
 		user.setEmail(email);
 		user.setEnabled(1);
 		user.setLocked(1);
-		userRepository.save(user);*/
-		return "redirect:register";
+		if (userRepository.findByEmail(user.getEmail()).isEmpty()) {
+			userRepository.save(user);
+			return "redirect:login";
+		}
+		else {
+			model.addAttribute("warning", "Email đã được sử dụng. Vui lòng sử dụng email khác");
+			model.addAttribute("fullName",user.getFullname());
+			return "user/register";
+		}
 	}
 
 }
--- SocialNetwork/src/main/java/com/SocialNetwork/Repository/UserRepository.java
+++ SocialNetwork/src/main/java/com/SocialNetwork/Repository/UserRepository.java
@@ -1,16 +1,16 @@
 package com.SocialNetwork.Repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.SocialNetwork.Entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-	
-	 	
+
+	List<User> findByEmail(String email);
 }
 
